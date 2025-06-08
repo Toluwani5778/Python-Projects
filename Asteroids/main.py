422227fea40a6d3c85cc811910b857882f85af52
Toluwani <tolukann5778@gmail.com>
2025-06-08 15:27:55 -0400

    update game

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -3,20 +3,36 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()  # Initialize the pygame library
+    clock = pygame.time.Clock()  # Create a clock object to manage frame rate
+    dt = 0  # Initialize delta time variable
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
 
+    updatable = pygame.sprite.Group()  # Create a group for updating sprites
+    drawable = pygame.sprite.Group()  # Create a group for drawing sprites
+    Player.containers = (updatable, drawable)  # Set the containers for the Player class
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)  # Create a player instance at the center of the screen
+
     while True:
         screen.fill((0, 0, 0))
+        
+        updatable.update(dt)  # Update the player state with the delta time
+        for draw in drawable:
+            draw.draw(screen) # Draw the player on the screen
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.display.flip()
+        # Limit the frame rate to 60 FPS
+        dt = clock.tick(120)/1000  # Update the display
+        
 
 if __name__ == "__main__":
     main()
