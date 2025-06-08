c3b458da80f34847e280b0c38665f2fc3da039f8
Toluwani <tolukann5778@gmail.com>
2025-06-07 20:01:20 -0400

    starting asteroids game

---
+++ Asteroids/main.py
@@ -0,0 +1,24 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+def main():
+    pygame.init()  # Initialize the pygame library
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Starting Asteroids!")
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
+
+    while True:
+        screen.fill((0, 0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
+
+ 
